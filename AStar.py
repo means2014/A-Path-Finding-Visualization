a0a2862b4b5f5cc7502f2b98197ffa45916db42e
Christopher Means <means2014@gmail.com>
2020-05-20 00:25:12 -0400

    There is currently some error with the draw method

--- AStar.py
+++ AStar.py
@@ -24,7 +24,6 @@ SCREEN_HEIGHT = 800
 SCREEN_WIDTH = SCREEN_HEIGHT #Lets keep things simple with regular hexagons
 NUM_COLS = 50
 NUM_ROWS = NUM_COLS
-GRID = [0 for i in range(NUM_COLS)]
 
 #Colors
 VISITED = (255,0,0)
@@ -78,12 +77,12 @@ class Node:
         
         #above
         n_i, n_j = i, j - 1
-        if i > 0 and not board[n_i][n_j].obstacle:
+        if j > 0 and not board[n_i][n_j].obstacle:
             self.neighbors.append(board[n_i][n_j])
         
         #below
         n_i, n_j = i, j + 1
-        if i < NUM_COLS - 1 and not board[n_i][n_j].obstacle:
+        if j < NUM_ROWS - 1 and not board[n_i][n_j].obstacle:
             self.neighbors.append(board[n_i][n_j])
         
         #top right
@@ -116,5 +115,171 @@ class Node:
             x_offset = w/(2*math.cos(math.pi/6))
             y_offset = h/(2*math.cos(math.pi/6))
             small_x_offset = (w/2)*math.tan(math.pi/6)
-            pygame.draw.polygon(screen, color, ([self.x+x_offset, self.y], [self.x+small_x_offset, self.y-y_offset], [self.x-small_x_offset, self.y-y-y_offset], [self.x-x_offset, self.y], [self.x-small_x_offset, self.y+y_offset], [self.x+small_x_offset, self.y+y_offset]), lineWidth)
+            pygame.draw.polygon(screen, color, ([self.x+x_offset, self.y], [self.x+small_x_offset, self.y-y_offset], [self.x-small_x_offset, self.y-y_offset], [self.x-x_offset, self.y], [self.x-small_x_offset, self.y+y_offset], [self.x+small_x_offset, self.y+y_offset]), lineWidth)
             pygame.display.update()
+
+# Initialize board
+GRID = [0 for i in range(NUM_COLS)]
+for i in range(NUM_COLS):
+    GRID[i] = [0 for j in range(NUM_ROWS)]
+    
+for i in range(NUM_COLS):
+    for j in range(NUM_ROWS):
+        GRID[i][j] = Node(i,j)
+
+# Function for setting start and finish points
+def onsubmit():
+    global start
+    global end
+    st = startBox.get().split(' ')
+    ed = endBox.get().split(' ')
+    start = GRID[int(st[0])][int(st[1])]
+    end = GRID[int(ed[0])][int(ed[1])]
+    window.quit()
+    window.destroy()
+
+# Function for creating obstacle points by dragging mouse
+def createObstacle(x):
+    obstacle = GRID[x[0] // (SCREEN_WIDTH // NUM_COLS)][x[1] // (SCREEN_HEIGHT // NUM_ROWS)]
+    if not (obstacle == start or obstacle == end or obstacle.obstacle):
+        obstacle.obstacle = True
+        obstacle.draw(OBSTACLE, 0)
+        
+# Define heuristic for A*
+def heuristic(p1, p2):
+    return math.sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2)
+    
+def main():
+    start.draw(START_FINISH, 0)
+    end.draw(START_FINISH, 0)
+    
+    if open:
+        lowestIndex = 0
+        for i in range(len(open)):
+            if open[i].f < open[lowestIndex].f:
+                lowestIndex = i
+                
+        current = open[lowestIndex]
+        if current == end:
+            print('done', current.f)
+            #start.draw(START_FINISH, 0)
+            temp = current.f
+            for i in range(round(current.f)):
+                current.closed = False
+                current.draw(IN_OPT,0)
+                current = current.previous
+            #end.draw(START_FINISH, 0)
+            
+            Tk().wm_withdraw()
+            result = messagebox.askokcancel('Goal Found!', ('The solution was found.\nThe total distance travelled was ' + str(temp) + '\nRun the program again?'))
+            if result:
+                os.execl(sys.executable, sys.executable, *sys.argv)
+            else:
+                game_over = True
+                while game_over:
+                    ev = pygame.event.get()
+                    for event in ev:
+                        if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+                            game_over = False
+                            break
+            pygame.quit()
+            
+        open.pop(lowestIndex)
+        closed.append(current)
+        
+        neighbors = current.neighbors
+        for neighbor in neighbors:
+            if neighbor not in closed:  
+                tempG = current.g + current.cost
+                if neighbor in open:
+                    if neighbor.g > tempG:
+                        neighbor.g = tempG
+                else:
+                    neighbor.g = tempG
+                    open.append(neighbor)
+                    
+            neighbor.h = heuristic(neighbor, end)
+            neighbor.f = neighbor.g + neighbor.h
+            
+            if neighbor.previous is None:
+                neighbor.previous = current
+    
+    if var.get():
+        for i in range(len(open)):
+            open[i].draw(NEIGHBORING, 0)
+            
+        for i in range(len(closed)):
+            if closed[i] != start:
+                closed[i].draw(VISITED, 0)
+    current.closed = True
+    
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen.fill(BACKGROUND)
+
+for i in range(NUM_COLS):
+    for j in range(NUM_ROWS):
+        GRID[i][j].draw(NOT_VISITED, 1)
+        
+for i in range(NUM_ROWS):
+    GRID[0][i].draw(OOB, 0)
+    GRID[0][i].obstacle = True
+    GRID[NUM_COLS-1][i].obstacle = True
+    GRID[NUM_COLS-1][i].draw(OOB, 0)
+    GRID[i][NUM_ROWS-1].draw(OOB, 0)
+    GRID[i][0].draw(OOB, 0)
+    GRID[i][0].obstacle = True
+    GRID[i][NUM_ROWS-1].obstacle = True
+
+window = Tk()
+label = Label(window, text = 'Starting point: x1 y1')
+startBox = Entry(window)
+label1 = Label(window, text = 'Ending point: x2 y2')
+endBox = Entry(window)
+var = IntVar()
+drawPath = ttk.Checkbutton(window, text="Display steps?", onvalue=1, offvalue=0, variable=var)
+
+submit = Button(window, text="Submit", command=onsubmit)
+
+drawPath.grid(columnspan=2, row=2)
+submit.grid(columnspan=2, row=3)
+label1.grid(row=1, pady=3)
+endBox.grid(row=1, column=1, pady=3)
+startBox.grid(row=0, column=1, pady=3)
+label.grid(row=0, pady=3)
+
+window.update()
+mainloop()
+
+pygame.init()
+open.append(start)
+
+start.draw(START_FINISH, 0)
+end.draw(START_FINISH, 0)
+
+loop = True
+while loop:
+    ev = pygame.event.get()
+    for event in ev:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if pygame.mouse.get_pressed()[0]:
+            try:
+                pos = pygame.mouse.get_pos()
+                createObstacle(pos)
+            except AttributeError:
+                pass
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                loop = False
+                break
+                
+for i in range(NUM_COLS):
+    for j in range(NUM_ROWS):
+        GRID[i][j].neighborhood(GRID)
+
+while True:
+    ev = pygame.event.poll()
+    if ev.type == pygame.QUIT:
+        pygame.quit()
+    pygame.display.update()
+    main()
